b00b6d16ee3a3d728d4079d75ddca70bf33edfe6
Starling Germosen <sgermosen@outlook.com>
2025-01-25 11:34:29 -0400

    Haciendo la tarea 2

--- src/Tarea2/WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ src/Tarea2/WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace WebApplication1.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        asas
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
--- src/Tarea2/WebApplication1/WebApplication1/Program.cs
+++ src/Tarea2/WebApplication1/WebApplication1/Program.cs
@@ -16,6 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+as
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
